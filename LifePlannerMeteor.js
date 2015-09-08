91d6fd72caeb81832b67b461c9db19a001b5c541
Sayan Faraz <sayan96@hotmail.com>
2015-09-08 01:41:00 -0400

    LifePlanner v0
    COMPLETED: Basic UI structure, adding tasks functionality (still a bit
    buggy)

---
+++ LifePlannerMeteor.js
@@ -0,0 +1,22 @@
+TasksDB = new Meteor.Collection("taskCol");
+
+if (Meteor.isClient) {
+    Template.tasksDisplayTemplate.helpers({
+        tasks_db_caller: function () {
+            return TasksDB.find();
+        }
+    });
+    
+    Template.tasksInputTemplate.events({
+        'submit form': function () {
+            var new_task = $('#newTask').val();
+            TasksDB.insert({task: new_task});
+        }
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+    // code to run on server at startup
+    });
+}
